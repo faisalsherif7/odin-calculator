949bfa7a00e86610526963fd35f77a5639de618d
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-07-11 16:56:47 +0530

    Create 'operate' function

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ function multiply(...numbers) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, op) {
+    if (op === '+') return add(a, b);
+    else if (op === '-') return subtract(a, b);
+    else if (op === '*') return multiply([a, b])
+    else if (op === '/') return divide(a, b)
+}
