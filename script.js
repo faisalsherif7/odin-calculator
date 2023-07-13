95bb59ddcde7b769b3d306efbff65ad0452531b5
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-07-13 15:43:55 +0530

    Create calculator that evaluates complete expression after hitting '='
    Misunderstood instructions and created a calculator that evaluates
    entire expression only after hitting '=' sign, applying BODMAS rule.

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
-function multiply(...numbers) {
-    return numbers.reduce((total, current) => {
-        return total * current;
-    }, 1)
+function multiply(a, b) {
+    return +a * +b;
 }
 
 function divide(a, b) {
-    return a / b;
+    return +a / +b;
 }
 
 function operate(a, b, op) {
     if (op === '+') return add(a, b);
     else if (op === '-') return subtract(a, b);
-    else if (op === '*') return multiply([a, b])
+    else if (op === '*') return multiply(a, b)
     else if (op === '/') return divide(a, b)
 }
 
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display')
-buttons.forEach(button => button.addEventListener('click', () => {
-    display.textContent += ' ' + button.textContent;
-}))
+const buttons = document.querySelectorAll('#calculator-button');
+const display = document.querySelector('.display');
+const equals = document.querySelector(".equals")
+const clear = document.querySelector(".clear")
+
+clear.addEventListener('click', () => display.textContent = '')
+buttons.forEach(button => button.addEventListener('click', () => display.textContent += button.textContent))
+
+equals.addEventListener('click', () => {
+
+    // Get the big number to be divided
+    let array = display.textContent.split('');
+
+    // If division available, do so
+    if (array.includes('/')) {
+        operateAll('/', array);
+    }
+
+    if (array.includes('*')) {
+        operateAll('*', array);
+    }
+
+    if (array.includes('+')) {
+        operateAll('+', array);
+    }
+
+    if (array.includes('-')) {
+        operateAll('-', array);
+    }
+
+    return display.textContent = array;
+})
+
+
+function operateAll(operator, array) {
+    let indexArray = array.reduce((indexArray, currentElement, currentIndex) => {
+        if (currentElement === operator) {
+            indexArray.push(currentIndex);
+        }
+        return indexArray;
+    }, [])
+    indexArray.map(index => {
+        let result = operate(array[index - 1], array[index + 1], operator);
+        array.splice(index - 1, 3, result);
+    })
+}
